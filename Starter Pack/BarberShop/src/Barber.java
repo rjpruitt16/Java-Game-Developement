5e1ca2eb3ee6e3a93c55721432bf712e31ab8219
Rahmi Pruitt <rjpruitt16@gmail.com>
2016-10-22 21:58:39 -0500

    I deleted all of my useless files
    Self Explanatory

--- Starter Pack/BarberShop/src/Barber.java
+++
@@ -1,8 +0,0 @@
-
-public class Barber {
-  private Costumer[] seats;
-  private boolean serving;
-  private static final int FULL = 0;
-  private static final int WAITING = 1;
-  private static final int Empty = 2;
-}
