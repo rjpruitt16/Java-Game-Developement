34ad60bda37f239d95eee0dad2396c67faf05f44
ElementsHeir <williams.d.tyree@gmail.com>
2016-10-22 22:31:46 -0500

    Revised classes: HUD, Handler and Game
    changed draw to render, intialized the variable and called it in the
    main function Game
    called HUD to render display on game screen

---
+++ Starter Pack/game/HUD.java
@@ -0,0 +1,33 @@
+package com.tutorial.game;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class HUD {
+
+	public static int HEALTH = 100;
+	
+	private int score = 0;
+	private int level = 1;
+	
+	public void tick() {
+		
+	}
+	
+	public void render(Graphics g)
+	{
+	  int maxHealth = 100;
+	  float healthScale = HEALTH / maxHealth;
+	  g.setColor(Color.red);
+	  g.fillRect(15, 15, 200, 30); //draws healthbar outline
+
+	  g.setColor(Color.green);
+	  int healthBarX = 15;
+	  float healthBarWidth = 200;
+	  int healthBarY = 15;
+	  int healthBarHeight = 30;
+	  g.fillRect(15, 15, HEALTH*2, 30); //draws health
+	}
+	
+
+}
