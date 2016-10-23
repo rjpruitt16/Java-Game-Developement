9ae67057eb266f4d4dd74145b4a72ee95ad6a34c
ElementsHeir <williams.d.tyree@gmail.com>
2016-10-22 21:57:43 -0500

    Created two seperate variables for player
    Player results in white square
    Enemy results in red square

--- Starter Pack/Player.java
+++ Starter Pack/Player.java
@@ -4,9 +4,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Random;
 
-public class Player extends GameObject {
-   Random r= new Random();
-   
+public class Player extends GameObject {	
+	
 	public Player(int x, int y, ID id) {
 		super(x, y, id);
 		
@@ -21,8 +20,15 @@ public class Player extends GameObject {
 	}
 	
 	public void render(Graphics g) {
-		if(id==ID.Player) g.setColor(Color.white);
-		else if( id== ID.Player2) g.setColor(Color.red);
+		if (id == ID.Player){
+		g.setColor(Color.white);
 		g.fillRect(x, y, 32, 32);
+		}
+		else{
+			g.setColor(Color.red);
+			g.fillRect(x,y, 32,32);
+			
+			
+		}
 	}
 }
